8221831ff028ffe4fbe73b9140ccba021d13c4d2
Vanessa <vanessa@Vanessas-MacBook-Pro.local>
2020-07-12 11:10:38 +0200

    Removing unused colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,6 +1,3 @@
-export const MINT = "#94dbc0";
-export const LIGHT_MINT = "#b6e7cb";
-export const WHITE_MINT = "#d9f2e6";
 export const WHITE_D = "#ddd";
 export const WHITE_C = "#CCC";
 export const GREY_2 = "#222";
